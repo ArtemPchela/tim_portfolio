5bf5cc3396c0348f70e75b78e5f58e5f95dd109e
ArtemPchela <pchelenkovartem@gmail.com>
2022-05-24 14:14:48 +0200

    was fixed project name

--- components/helpers/data/projectsData.ts
+++ components/helpers/data/projectsData.ts
@@ -2,7 +2,7 @@ import { ProjectProps } from '@/components/projects/project/Project'
 
 export const projectsData: ProjectProps[] = [
   {
-    title: 'RÖVARSPRÅK',
+    title: 'RÖVARSPRÅKET',
     description: 'Rövarspråket (English: The Robber Language) is a Swedish language game. It became popular after the books about Bill Bergson by Astrid Lindgren, where the children use it as a code, both at play and in solving actual crimes.',
     technologies: ['React', 'CSS', 'i18n', 'PWA'],
     github: 'https://github.com/ArtemPchela/Rovarspraket_Game',
